0e0d9607cca523832e3d78a3c9f7eaa240cb2bd4
juxsalley <juxsalley@gmail.com>
2021-05-17 20:29:33 +0000

    implemet active color for all nav items

--- main/components/NavBar.js
+++ main/components/NavBar.js
@@ -1,47 +1,68 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
-import {Link} from 'react-router-native';
+import {Link, useLocation} from 'react-router-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const NavBar = () => (
-  <View style={navStye.mainNav}>
-    <Link to="/">
-      <>
-        <Text>Wallet</Text>
-        <Text>Wallet</Text>
-      </>
-    </Link>
-    <Link to="/cards">
-      <>
-        <Text>cards</Text>
-        <Text>cards</Text>
-      </>
-    </Link>
-    <Link to="/prices">
-      <>
-        <Text>prices</Text>
-        <Text>prices</Text>
-      </>
-    </Link>
-    <Link to="/more">
-      <>
-        <Text>More</Text>
-        <Text>More</Text>
-      </>
-    </Link>
-  </View>
-);
+Icon.loadFont();
+
+const NavBar = () => {
+  const setColor = inPath => {
+    const path = useLocation().pathname;
+    if (path === inPath) {
+      return navStye.activeNavColor;
+    } else {
+      return navStye.disableNavColor;
+    }
+  };
+
+  return (
+    <View style={navStye.mainNav}>
+      <Link to="/" underlayColor="white">
+        <>
+          <Icon name="account-balance-wallet" size={30} style={setColor('/')} />
+          <Text style={setColor('/')}>Wallet</Text>
+        </>
+      </Link>
+      <Link to="/cards" underlayColor="white">
+        <>
+          <Icon name="credit-card" size={30} style={setColor('/cards')} />
+          <Text style={setColor('/cards')}>cards</Text>
+        </>
+      </Link>
+      <Link to="/prices" underlayColor="white">
+        <>
+          <Icon name="bar-chart" size={30} style={setColor('/prices')} />
+          <Text style={setColor('/prices')}>prices</Text>
+        </>
+      </Link>
+      <Link to="/more" underlayColor="white">
+        <>
+          <Icon name="more-horiz" size={30} style={setColor('/more')} />
+          <Text style={setColor('/more')}>More</Text>
+        </>
+      </Link>
+    </View>
+  );
+};
 
 const navStye = StyleSheet.create({
   mainNav: {
     position: 'absolute',
     bottom: 0,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     width: '100%',
     paddingBottom: '5%',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: '5%',
+    paddingVertical: 10,
+  },
+  activeNavColor: {
+    color: 'blue',
+  },
+  disableNavColor: {
+    color: 'gray',
   },
 });
 
